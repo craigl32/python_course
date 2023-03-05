b93a6962d67b44f68a8a50a0fd83ba3fe41428eb
craigl32 <craiglevit@gmail.com>
2023-03-05 16:45:09 +0200

    Commit AppBrewery.py

--- leap_year.py
+++
@@ -1,7 +0,0 @@
-year = int(input("Enter year: "))
-if year % 4 == 0:
-    print("Leap year")
-elif year != 100:
-    print("Not a leap year")
-elif  year % 400  == 0:
-    print("Leap year")
--- tip_calc.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the bill calculator!")
-bill = float(input("What was the total bill? $"))
-tip = float(input("How much tip would you like to give? 10, 12 or more? "))
-people = int(input("How many people would you like to split the bill ? "))
-bill_with_tip = bill * (tip / 100) + bill
-print(bill_with_tip)
-bill_per_person = round(bill_with_tip / people, 2)
-print(f"Each person should pay {bill_per_person}")
